cffe9735ddda5c36887d9a5a8bc600a11d41ba33
Sam Pennington <sam.pennington@haymarket.com>
2019-05-13 20:27:21 +0100

    update git ignore, add proptype

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+
 import "./ErrorMessage.css";
 
 const ErrorMessage = ({ error }) => {
   const [dismissed, setDismissed] = useState(false);
 
   return (
-    error && !dismissed
+    !dismissed
       ? <div className="error">{error} <span onClick={setDismissed}>X</span></div>
       : null
   );
+};
+
+ErrorMessage.propTypes = {
+  error: PropTypes.string
 };
 
 export default ErrorMessage;
